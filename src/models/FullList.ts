bf7502c5b76c05d2d4b229f56a34d9f63d42c57c
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-10-30 02:31:03 +0100

    h

--- src/models/FullList.ts
+++ src/models/FullList.ts
@@ -1,7 +1,7 @@
 import ListItem from "./ListItem";
 
-interface List {
-    List: ListItem[],
+interface Lists {
+    list: ListItem[],
     load(): void,
     save(): void,
     clearList(): void,
@@ -9,9 +9,31 @@ interface List {
     removeItem(id: string): void
 }
 
-export default class FullList implements List {
+export default class FullList implements Lists {
     static instance: FullList = new FullList()
 
-    private constructor(private _List: ListItem[] = []) {}
+    private constructor(private _list: ListItem[] = []) {}
+
+    get list(): ListItem[] {
+        return this._list
+    }
+    // set list(list: ListItem[]){
+    //     this._list = list
+    // }
     
+    save(): void {
+        localStorage.setItem("myItem", JSON.stringify(this._list))
+    }
+    clearList(): void {
+        this._list = []
+        this.save()
+    }
+    addItem(itemObj: ListItem): void {
+        this._list.push(itemObj)
+        this.save()
+    }
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id)
+        this.save()
+    }
 }
