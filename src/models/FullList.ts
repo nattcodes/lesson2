f78880a7420fda52a737e4ba99fe3c56b77495ad
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-10-30 02:42:13 +0100

    h

--- src/models/FullList.ts
+++ src/models/FullList.ts
@@ -20,18 +20,37 @@ export default class FullList implements Lists {
     // set list(list: ListItem[]){
     //     this._list = list
     // }
-    
+    load(): void {
+        const storedList: string | null = localStorage.getItem("myItem")
+        if (typeof storedList !== "string") return
+
+        const parsedList: {
+            _id: string,
+            _item: string,
+            _checked: boolean
+        }[] = JSON.parse(storedList)
+
+        parsedList.forEach( itemObj => {
+            const newListItem = new ListItem( itemObj._id, itemObj._item, itemObj._checked)
+            FullList.instance.addItem(newListItem)
+        })
+        
+    }
+
     save(): void {
         localStorage.setItem("myItem", JSON.stringify(this._list))
     }
+    
     clearList(): void {
         this._list = []
         this.save()
     }
+
     addItem(itemObj: ListItem): void {
         this._list.push(itemObj)
         this.save()
     }
+
     removeItem(id: string): void {
         this._list = this._list.filter(item => item.id !== id)
         this.save()
