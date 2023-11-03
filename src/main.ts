1e51af90ccdc4e8b5ee7d57b4df5931549561655
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-11-03 20:25:23 +0100

    d

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import './style.css'
 const list = document.getElementById('list') as HTMLUListElement
 const form = document.getElementById('form') as HTMLFormElement
 const inputText = document.getElementById('form-input-text') as HTMLInputElement
-const btn = document.getElementById('btn') as HTMLInputElement
 
-form?.addEventListener("submit", e => {
+form.addEventListener("submit", e => {
     e.preventDefault()
-    console.log("e")
+    if (inputText.value == "" || inputText.value == null) return
+
+
+    const task = {
+        
+        title: inputText.value,
+        completed: false,
+        createdAt: new Date()
+    }
 })
