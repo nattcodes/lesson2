b5a82ae047137e231f133ac7c73eed9c11a028be
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-11-03 20:07:25 +0100

    j

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-
 import './style.css'
+
+const list = document.getElementById('list') as HTMLUListElement
+const form = document.getElementById('form') as HTMLFormElement
+const inputText = document.getElementById('form-input-text') as HTMLInputElement
+const btn = document.getElementById('btn') as HTMLInputElement
+
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+    console.log("e")
+})
